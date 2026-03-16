c6085f0f8e803d9a1eb751176cc7d80dd01a8219
EmmanuelJYokoyama <emmanuelnoronha2@gmail.com>
2026-03-16 09:33:43 -0300

    fix: update environment variables and improve MongoDB connection handling

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 // src/app.ts
+import 'dotenv/config';
 import fastify from 'fastify';
 import fastifyStatic from '@fastify/static';
 import path from 'path';
@@ -7,11 +8,8 @@ import { routes } from './routes';
 
 const app = fastify({ logger: true });
 
-const mongoURI = process.env.DATABASE_URL || 'mongodb://admin:senhamongo@mongodb:27017/apinode?authSource=admin';
-
-mongoose.connect(mongoURI)
-  .then(() => console.log('MongoDB Conectado!'))
-  .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
+const mongoURI = process.env.DATABASE_URL || 'mongodb://root:admin@localhost:27017/apinode?authSource=admin';
+const port = Number(process.env.PORT || 3000);
 
 app.register(fastifyStatic, {
   root: path.join(__dirname, '..', 'public'),
@@ -22,8 +20,10 @@ app.register(routes);
 
 const start = async () => {
   try {
-    await app.listen({ port: 300l0, host: '0.0.0.0' });
-    console.log('Servidor rodando na porta 3000');
+    await mongoose.connect(mongoURI);
+    console.log('MongoDB Conectado!');
+    await app.listen({ port, host: '0.0.0.0' });
+    console.log(`Servidor rodando na porta ${port}`);
   } catch (err) {
     app.log.error(err);
     process.exit(1);
